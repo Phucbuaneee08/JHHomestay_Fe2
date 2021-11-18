0836deb07d5c3e7d03fe34f9084c7e1b27d02ebb
Vũ Minh Thanh <vuminhthanh8501@gmail.com>
2021-11-18 13:55:08 +0700

    Thanh: Fix animation Error
    - Add check for empty Email and Password
    - Fix animation Error when popup message
    - Optimize css, remove unused

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -11,7 +11,11 @@ function Auth() {
         email: "",
         password: ""
     })
-    const [errorState, setErrorState] = useState(false);
+    const [errorState, setErrorState] = useState({
+        errorValue: false,
+        errorName: ""
+    });
+    const [loading, setLoading] = useState(false);
     const dispatch = useDispatch();
 
     /* Handler */
@@ -24,49 +28,72 @@ function Auth() {
 
     const authHandler = async (e) => {
         e.preventDefault();
-        try {
-            const response = await axios.post("http://localhost:8000/auth/login", authData);
-            dispatch({
-                type: "LOGIN",
-                payload: {
-                    userId:response.data.content._id,
+        setErrorState({
+            errorValue: false, 
+            errorName: ""
+        });
+        setLoading(true);
+
+        if (authData.email === "") {
+            setErrorState({
+                errorValue: true,
+                errorName: "Chưa điền địa chỉ Email"
+            });
+            setLoading(false);
+        } else if (authData.password === "") {
+            setErrorState({
+                errorValue: true,
+                errorName: "Chưa điền Mật khẩu"
+            });
+            setLoading(false);
+        } else {
+            try {
+                const response = await axios.post("http://localhost:8000/auth/login", authData);
+                dispatch({
+                    type: "LOGIN",
+                    payload: {
+                        userId:response.data.content._id,
+                        token: response.data.content.token
+                    }
+                })
+                localStorage.setItem("authData", JSON.stringify({
+                    userId: response.data.content._id,
                     token: response.data.content.token
-                }
-            })
-            localStorage.setItem("authData", JSON.stringify({
-                userId: response.data.content._id,
-                token: response.data.content.token
-            }))
-        } catch (err) {
-            setErrorState(true);
+                }))
+            } catch (err) {
+                setErrorState({
+                    errorValue: true,
+                    errorName: "Email hoặc Mật khẩu không đúng"
+                });
+            }
+            setLoading(false);
         }
     }
 
     return (
         <>
-            <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-                <div className="max-w-md w-full space-y-8">
+            <div className="relative overflow-hidden w-screen min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+                <div className="w-md space-y-8">
                     <div>
                         <img
                             className="mx-auto h-24 w-auto"
                             src={logo}
                             alt="Logo"
                         />
-                        <h2 className="text-center text-3xl font-bold text-gray-900 pt-2">Sign in to your account</h2>
+                        <h2 className="text-center text-3xl font-bold text-gray-900 pt-2">Đăng nhập vào tài khoản của bạn</h2>
                     </div>
                     <div className="w-full bg-white p-8 border border-gray-200 rounded-lg shadow-md">
                         <form className="space-y-6" method="POST">
                             <div className="rounded-lg shadow-sm">
                                 <div>
                                     <label htmlFor="email-address">
-                                        Email address
+                                        Địa chỉ Email
                                     </label>
                                     <input 
                                         id="email-address"
                                         name="email"
                                         type="email"
                                         autoComplete="email"
-                                        value={authData.email}
                                         required
                                         className="appearance-none rounded-none relative block w-full px-3 py-2 mt-1 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-green-800 focus:border-green-800 focus:z-10 sm:text-sm"
                                         onChange={handleInputChange}
@@ -74,7 +101,7 @@ function Auth() {
                                 </div>
                                 <div className="mt-4">
                                     <label htmlFor="password">
-                                        Password
+                                        Mật khẩu
                                     </label>
                                     <input 
                                         id="password"
@@ -97,12 +124,12 @@ function Auth() {
                                         className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
                                     />
                                     <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                                        Remember me
+                                        Lưu đăng nhập
                                     </label>
                                 </div>
                                 <div className="text-sm">
                                     <a href="#" className="font-medium text-green-600 hover:text-green-700">
-                                        Forgot your password?
+                                        Quên mật khẩu?
                                     </a>
                                 </div>
                             </div>
@@ -113,27 +140,27 @@ function Auth() {
                                     onClick={authHandler}
                                 >
                                     <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                                        <LockClosedIcon className="h-5 w-5 text-green-600 group-hover:text-green-900" aria-hidden="true"/>
+                                        {loading ? 
+                                            <div className="border-top-transparent w-5 h-5 border-4 border-green-400 border-solid rounded-full animate-spin"/> :
+                                            <LockClosedIcon className="h-5 w-5 text-green-600 group-hover:text-green-900" aria-hidden="true"/>
+                                        }
                                     </span>
-                                    Sign in
+                                    Đăng nhập
                                 </button>
                             </div>
                         </form>
                     </div>
                 </div>
-            </div>
-            <div role="alert" className={`mt-16 sm:mt-6 mb-6 sm:mb-0 absolute left-0 sm:left-auto right-0 sm:top-0 flex items-center justify-between bg-white dark:bg-gray-800 shadow-lg rounded transition ease-in-out ${errorState ? 'p-4 mx-auto sm:mr-6 xl:w-1/4 sm:w-2/5 md:w-1/2 w-11/12 translate-show' : 'translate-hide w-0 overflow-hidden'}`}>
-                <div className="sm:px-2 p-1 mt-1 sm:mt-0 ml-2 sm:ml-0 flex items-center h-5 sm:h-auto text-red-500">
-                    <ExclamationCircleIcon className="w-5 h-5 mr-1"/>
-                    Wrong Email or Password
+                <div role="alert" className={`mt-6 absolute top-0 right-0 flex items-center justify-between bg-white shadow-md rounded-md transition ease-in-out p-2 mx-1 sm:mr-6 xl:w-1/4 md:w-1/3 sm:w-2/5 w-4/5 ${errorState.errorValue ? 'translate-show' : 'translate-hide'}`}>
+                    <div className="sm:px-2 p-1 mt-1 sm:mt-0 ml-2 sm:ml-0 flex items-center h-5 sm:h-auto text-red-500">
+                        <ExclamationCircleIcon className="w-5 h-5 mr-1"/>
+                        {errorState.errorName}
+                    </div>
+                    <button onClick={() => setErrorState(false)}>
+                        <XIcon className="w-5 h-5"/>
+                    </button>
                 </div>
-                <button 
-                    onClick={() => setErrorState(false)}
-                >
-                    <XIcon className="w-5 h-5"/>
-                </button>
             </div>
-
             <style>
                 {`
                     .translate-show {
@@ -141,6 +168,9 @@ function Auth() {
                     }
                     .translate-hide {
                         transform: translateX(120%);
+                    }
+                    .border-top-transparent {
+                        border-top: 4px solid rgba(0, 0, 0, 0);
                     }
                 `}
             </style>

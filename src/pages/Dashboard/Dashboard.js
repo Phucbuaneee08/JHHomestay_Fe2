93e7ab62f19a0662841ae7c0459e474ef24df740
Nguyễn Quỳnh Anh <quynhanh130701@gmail.com>
2021-11-19 17:47:30 +0700

    QA: sửa class thành className

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -11,31 +11,31 @@ import {Switch, Route, Redirect} from "react-router-dom"
 
 function Dashboard() {
     return(
-        <div class="grid grid-cols-7">
-            <Sidebar class="col-span-1"/>
-            <div class="col-start-2 col-end-8 flex-shrink bg-white">
+        <div className="grid grid-cols-7">
+            <Sidebar className="col-span-1"/>
+            <div className="col-start-2 col-end-8 flex-shrink bg-white">
                 <Header />
-            <Switch>
-                <Route path="/dashboard/mainpage" exact>
-                    <Mainpage />
-                </Route>
-                <Route path="/dashboard/sales" exact>
-                    <Sales />
-                </Route>
-                <Route path="/dashboard/reservation-management" exact>
-                    <Reservation />
-                </Route>
-                <Route path="/dashboard/homestay-management" exact>
-                    <Homestay />
-                </Route>
-                <Route path="/dashboard/user-management" exact>
-                    <User />
-                </Route>
-                <Route path="/dashboard/setting" exact>
-                    <Setting />
-                </Route>
-                <Redirect path="/dashboard/mainpage"/>
-            </Switch>
+                <Switch>
+                    <Route path="/dashboard/mainpage" exact>
+                        <Mainpage />
+                    </Route>
+                    <Route path="/dashboard/sales" exact>
+                        <Sales />
+                    </Route>
+                    <Route path="/dashboard/reservation-management" exact>
+                        <Reservation />
+                    </Route>
+                    <Route path="/dashboard/homestay-management" exact>
+                        <Homestay />
+                    </Route>
+                    <Route path="/dashboard/user-management" exact>
+                        <User />
+                    </Route>
+                    <Route path="/dashboard/setting" exact>
+                        <Setting />
+                    </Route>
+                    <Redirect path="/dashboard/mainpage"/>
+                </Switch>
             </div>    
         </div>
     )

3aaea119ec75145bdc60c41303a8625328f8be2f
Nguyễn Quỳnh Anh <quynhanh130701@gmail.com>
2021-12-24 16:53:42 +0700

    QA: Thêm pagination - perPage=15

--- src/components/Homestay/HomestayCard.js
+++ src/components/Homestay/HomestayCard.js
@@ -4,25 +4,25 @@ function HomestayCard (homestay){
         <tr>
             <td className="px-6 py-4 whitespace-nowrap">
                 <div className="flex items-center">
-                    <div className="text-sm font-medium text-gray-900">{name}</div>
+                    <div className="text-base font-medium text-gray-900">{name}</div>
                 </div>
             </td>
             <td className="px-6 py-4 whitespace-nowrap">
-                <div className="text-sm text-gray-900">{province}</div>
+                <div className="text-base text-gray-900">{province}</div>
                 <div className="text-sm text-gray-500">{district}</div>
             </td>
             <td className="px-6 py-4 whitespace-nowrap">
                 {hasAdmin
                     ?(<span 
                         className="
-                        px-2 inline-flex text-center text-xs leading-5 font-semibold rounded-full 
+                        px-2 inline-flex text-center text-sm leading-5 font-semibold rounded-full 
                       bg-green-100 text-green-800
                       ">
                         Đã có admin
                     </span>) 
                     :(<span 
                         className="
-                        px-2 inline-flex text-center text-xs leading-5 font-semibold rounded-full 
+                        px-2 inline-flex text-center text-sm leading-5 font-semibold rounded-full 
                         bg-red-100 text-red-800
                       ">
                         Chưa có admin
@@ -31,11 +31,11 @@ function HomestayCard (homestay){
             </td>
             <td> 
                 {hasAdmin
-                ? (<p className="px-6 py-4 whitespace-nowrap text-sm text-gray-500"> {admin.email} </p>)
+                ? (<p className="px-6 py-4 whitespace-nowrap text-base text-gray-500"> {admin.email} </p>)
                 : null}
             </td>
             {/* <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{_id}</td> */}
-            <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+            <td className="px-6 py-4 whitespace-nowrap text-right text-base font-medium">
                 <a href="!#" className="text-indigo-600 hover:text-indigo-900">
                 Cập nhật
                 </a>
--- src/components/Homestay/HomestayTable.js
+++ src/components/Homestay/HomestayTable.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
 
+import Pagination from "./Pagination";
 import HomestayCard from "./HomestayCard"
 
 const columnName = [
@@ -11,11 +12,25 @@ const columnName = [
 ]
 function HomestayTable(){
     const [data, setData] = useState([])
-    
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPage, setTotalPage] = useState(null)
+    const perPage=15
+
     useEffect(() => {
       const fetchData = async() => {
           try {
               const {data: response} = await axios.get('http://localhost:8000/super-admins/homestays');
+              setTotalPage(Math.ceil(response.content.length/perPage))
+          } 
+          catch (error) {
+              console.error(error.message);
+            };
+
+          try {
+              const {data: response} = await axios.get('http://localhost:8000/super-admins/homestays',{
+                params: {page: currentPage, perPage: perPage}
+            } );
+             
               setData(response.content)
               console.log(response.content)
           } 
@@ -24,18 +39,20 @@ function HomestayTable(){
             }
       }
       fetchData()
-  }, [])
+    }, [currentPage, perPage])
+
+    const paginate = pageNumber => setCurrentPage(pageNumber);
 
     return (
-        <div class="container my-8 pb-10">
-          <div className="w-11/12 mx-auto shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+        <div class="container my-8 pb-10 mx-auto">
+          <div className="w-11/12 mx-auto shadow overflow-hidden border-b border-gray-200 rounded-lg">
             <table className="w-full divide-y divide-gray-200">
               <thead className="bg-gray-50">
                 <tr>
                   {columnName.map(item => (
                       <th
                     scope="col"
-                    className="sticky px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    className="sticky px-6 py-3 text-left text-base font-medium text-gray-500 uppercase tracking-wider"
                     >
                     {item.name} 
                     </th>
@@ -56,6 +73,12 @@ function HomestayTable(){
               </tbody>
             </table>
           </div>
+          
+          <Pagination
+                totalPage={totalPage}
+                paginate={paginate}
+            />
+          <p className="text-center text-green-800"> Page {currentPage} of {totalPage} </p>
         </div>
     )
 }

459af454bc09587752ac11506e4af075cd980f63
Vu Phuonw <thephuong161002@gmail.com>
2023-02-23 17:30:59 +0700

    aa

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,7 @@
 // craco.config.js
 module.exports = {
     style: {
-      postcss: {
+      postOptionss: {
         plugins: [
           require('tailwindcss'),
           require('autoprefixer'),
